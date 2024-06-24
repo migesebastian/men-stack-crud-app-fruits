cafe661a0a73044ead70a349e6f16413964709c8
mmarrone97 <mmarrone97@gmail.com>
2024-06-24 11:01:13 -0400

    delete option update

--- server.js
+++ server.js
@@ -4,58 +4,66 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
- const app = express()
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+
+const app = express()
 
 mongoose.connect(process.env.MONGODB_URI);
 
 
 mongoose.connection.on("connected", () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 // Import the Fruit model
 const Fruit = require("./models/fruit.js");
 
 //middleware
-
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('method'));
+app.use(morgan('dev'));
 
 // GET /
 app.get("/", async (req, res) => {
     res.render("index.ejs");
-  });
+});
 
 // GET /fruits
 app.get("/fruits", async (req, res) => {
     const allFruits = await Fruit.find();
     console.log(allFruits); // log the fruits!
     res.render('fruits/index.ejs', { fruits: allFruits });
-  });
-  
-  
-  // GET /fruits/new
+});
+
+
+// GET /fruits/new
 app.get("/fruits/new", (req, res) => {
     res.render('fruits/new.ejs');
-  });
+});
 
-  app.get("/fruits/:fruitId", async (req, res) => {
+app.get("/fruits/:fruitId", async (req, res) => {
     const foundFruit = await Fruit.findById(req.params.fruitId);
     res.render("fruits/show.ejs", { fruit: foundFruit });
-  });
-  
-  
+});
+
+
 // POST /fruits
 app.post("/fruits", async (req, res) => {
     if (req.body.isReadyToEat === "on") {
-      req.body.isReadyToEat = true;
+        req.body.isReadyToEat = true;
     } else {
-      req.body.isReadyToEat = false;
+        req.body.isReadyToEat = false;
     }
     await Fruit.create(req.body);
     res.redirect("/fruits"); // redirect to index fruits
+});
+
+app.delete("/fruits/:fruitId", async (req, res) => {
+    await Fruit.findByIdAndDelete(req.params.fruitId);
+    res.redirect("/fruits");
   });
   
-
-  app.listen(3000, () => {
+app.listen(3000, () => {
     console.log("Listening on port 3000");
-  });
+});
