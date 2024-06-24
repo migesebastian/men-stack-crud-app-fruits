ee295ed11c9ae1d81b5529bc460fa8b60ee692e3
mmarrone97 <mmarrone97@gmail.com>
2024-06-24 11:46:29 -0400

    complete

--- server.js
+++ server.js
@@ -47,12 +47,29 @@ app.get("/fruits/:fruitId", async (req, res) => {
     res.render("fruits/show.ejs", { fruit: foundFruit });
 });
 
+
 // GET localhost:3000/fruits/:fruitId/edit
 app.get("/fruits/:fruitId/edit", async (req, res) => {
     const foundFruit = await Fruit.findById(req.params.fruitId);
     res.render("fruits/edit.ejs", {
       fruit: foundFruit,
     });
+  });
+  
+
+app.put("/fruits/:fruitId", async (req, res) => {
+    // Handle the 'isReadyToEat' checkbox data
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    
+    // Update the fruit in the database
+    await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+  
+    // Redirect to the fruit's show page to see the updates
+    res.redirect(`/fruits/${req.params.fruitId}`);
   });
   
 // POST /fruits
